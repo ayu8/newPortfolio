9aef64c1c755d84d90e45fe1afe81cc792c44e42
Ayush Singh <singhayush873@gmail.com>
2022-08-17 13:36:22 +0530

    Switching between sections: done

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const sections = document.getElementsByClassName('section');
-const sectBtns = document.getElementsByClassName('controls-container');
-const sectBtn = document.getElementsByClassName('controls');
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls-container');
+const sectBtn = document.querySelectorAll('.control');
 
-const mainSect = document.getElementById('main');
+const allSections = document.querySelector('.main-content');
 
 
 function pageTransitions() {
@@ -10,30 +10,30 @@ function pageTransitions() {
 
     for (let i = 0; i < sectBtn.length; i++) {
         sectBtn[i].addEventListener('click', function() {
-            let currentBtn = document.getElementsByClassName('active-btn');
+            let currentBtn = document.querySelectorAll('.active-btn');
             currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
             this.className += ' active-btn';
         })
     }
 
-    // sections active
-    mainSect.addEventListener('click', (e) => {
+    // sections active class
+    allSections.addEventListener('click', (e) => {
         const id = e.target.dataset.id;
 
         if (id) {
             //removing selected from other buttons
             sectBtns.forEach((btn) => {
-                btn.className.remove('active');
+                btn.classList.remove('active');
             })
-            e.target.className.add('active');
+            e.target.classList.add('active');
 
             // hide other sections
             sections.forEach((section) => {
-                section.className.remove('active');
+                section.classList.remove('active');
             })
 
             const element = document.getElementById(id);
-            element.className.add('active');
+            element.classList.add('active');
         }
     })
 }
