1ac9478a7af9cbe797999e86e69b43f849caf79d
Ayush Singh <singhayush873@gmail.com>
2022-08-24 20:43:01 +0530

    fixed View Resume button

--- app.js
+++ app.js
@@ -7,7 +7,22 @@
             document.getElementById(button.dataset.id).classList.add("active");
         })
     });
+
     document.querySelector(".theme-btn").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
     })
+    
+    
 })();
+
+document.querySelectorAll(".view-resume").forEach(ele => {
+    ele.addEventListener("click", function() {
+        document.querySelectorAll(".control").forEach(button => {
+
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            document.getElementsByClassName("ctrl-res")[0].classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById("resume").classList.add("active");
+        })
+    })
+});
